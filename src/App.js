f7b1322fd6c8c642d5c2bbb5ec802706bdfde155
Tito <teetoegeonu@gmail.com>
2020-10-28 12:49:30 +0100

    ADD - Using moment library to get current time

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import NameTag from './NameTag';
 const App =  () => (
     <div class="container p-4">
-        <NameTag name="Egeonu Tito" />
+        <NameTag name="Egeonu Tito" duration={2}/>
     </div>
 )
 
--- src/NameTag.js
+++ src/NameTag.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import moment from 'moment';
+
 
 class NameTag extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            expiresAt: 0,
-            currentTime: 0,
+            expiresAt: moment().add(props.duration, "minutes"),
+            currentTime: moment(),
             timeLeft: 0
         }
     }
@@ -16,9 +18,8 @@ class NameTag extends React.Component {
             <div className="card mx-auto p-4" style={{width: '400px'}}>
                 <div className="text-center">
                     <h1>{this.props.name}</h1>
-                    <small>Expires in {this.state.expiresAt}</small> <br/>
-                    <small>Current in {this.state.currentTime}</small> <br/>
-
+                    <small>Expires in {this.state.expiresAt.format('MMM Do, h:mm:ss a')}</small> <br/>
+                    <small>Current time in {this.state.expiresAt.format('MMM Do, h:mm:ss a')}</small> <br/>
                 </div>
             </div>
         )
