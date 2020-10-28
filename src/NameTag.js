07984f11aae93cbe3ea692d339110204a993ec88
Tito <teetoegeonu@gmail.com>
2020-10-28 13:01:34 +0100

    ADD - added feauture to update current time every second

--- src/NameTag.js
+++ src/NameTag.js
@@ -13,13 +13,22 @@ class NameTag extends React.Component {
         }
     }
 
+    componentDidMount() {
+        // update currentTime every second (1000ms == 1s)
+        setInterval(() => {
+            this.setState({
+                currentTime: this.state.currentTime.add(1, 'seconds')
+            }) 
+        }, 1000)
+    }
+
     render() {
         return (
             <div className="card mx-auto p-4" style={{width: '400px'}}>
                 <div className="text-center">
                     <h1>{this.props.name}</h1>
                     <small>Expires in {this.state.expiresAt.format('MMM Do, h:mm:ss a')}</small> <br/>
-                    <small>Current time in {this.state.expiresAt.format('MMM Do, h:mm:ss a')}</small> <br/>
+                    <small>Current time in {this.state.currentTime.format('MMM Do, h:mm:ss a')}</small> <br/>
                 </div>
             </div>
         )
